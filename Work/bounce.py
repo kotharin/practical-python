9d7b14b1b33133614d6a968106c837e2cffa478b
Neerav Kothari <neerav.kothari@gmail.com>
2024-02-12 17:29:20 -0500

    lesson 1.1

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,17 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+
+decrement = 0.6
+
+total_bounces = 10
+
+bounce = 1
+
+while (bounce <= total_bounces):
+
+    height = height*decrement
+    print(bounce, round(height, 4))
+    bounce = bounce + 1
+
